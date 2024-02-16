10a82e21c8f41f34e93e3535bcbcacc57c4d4b4e
Andre Adipraja <nielraja123@gmail.com>
2024-02-16 21:29:15 +0700

    Add save with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,37 @@
-import express, { response } from "express";
+import express, { request, response } from "express";
 import {PORT, mongoDBURL} from "./config.js"
 import mongoose from "mongoose";
+import { Book } from "./models/bookModels.js";
 const app = express();
+app.use(express.json())
 app.get('/',(request,response)=>{
     console.log(request)
     return response.status(234).send('Testing')
 })
+
+app.post('/books', async (request, response)=>{
+    try {
+        if(
+            !request.body.title||
+            !request.body.author||
+            !request.body.publishedYear
+        ){
+            return response.status(400).send({
+                message:'Send all required fields: title, author, publishedYear'
+            })
+        }const newBook={
+            title:request.body.title,
+            author:request.body.author,
+            publishedYear:request.body.publishedYear,
+        };
+        const book = await Book.create(newBook);
+        return response.status(201).send(book)
+    }catch(error){
+        console.log(error.message)
+        response.status(500).send({message:error.message})
+    }
+})
+
 mongoose.connect(mongoDBURL).then(()=>{
     console.log('App connected to database');
     app.listen(PORT,()=>{
