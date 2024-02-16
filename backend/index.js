a62e5c33ec171b96efcdbb18c547b3f07f75e9e3
Andre Adipraja <nielraja123@gmail.com>
2024-02-16 22:19:31 +0700

    Add update book by id

--- backend/index.js
+++ backend/index.js
@@ -54,6 +54,28 @@ app.get('/books/:id',async(request,response)=>{
         response.status(500).send({message:error.message})
     }
 })
+app.put('/books/:id',async(request,response)=>{
+    try{
+        if(
+            !request.body.title||
+            !request.body.author||
+            !request.body.publishedYear
+        ){
+            return response.status(400).send({
+                message:"Send all required fields: title, author, publishedYear"
+            })
+        }
+        const {id}=request.params;
+        const result=await Book.findByIdAndUpdate(id, request.body)
+        if(!result){
+            return response.status(400).json({message:"Book not found"})
+        }
+        return response.status(200).send({message:"Book succesfully updated"})
+    }catch(error){
+        console.log(error.message)
+        response.status(500).send({message:error.message})
+    }
+})
 mongoose.connect(mongoDBURL).then(()=>{
     console.log('App connected to database');
     app.listen(PORT,()=>{
