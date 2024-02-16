3161f7e42a8c1dfbfaca68bd369254f88b0630f8
Andre Adipraja <nielraja123@gmail.com>
2024-02-16 12:10:37 +0700

    Add First HTTP Route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
-import express from "express";
+import express, { response } from "express";
 import {PORT} from "./config.js"
 const app = express();
+app.get('/',(request,response)=>{
+    console.log(request)
+    return response.status(234).send('Testing')
+})
 app.listen(PORT,()=>{
     console.log(`App is listening to port: ${PORT}`);
 })
