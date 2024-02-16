5e15eb75e3eabca78977d628469e11fe563901ec
Andre Adipraja <nielraja123@gmail.com>
2024-02-16 21:50:45 +0700

    Add find book by id

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,29 @@ app.post('/books', async (request, response)=>{
         response.status(500).send({message:error.message})
     }
 })
+app.get('/books',async(request,response)=>{
+    try{
+        const books=await Book.find({});
+        return response.status(200).json({
+            count:books.length,
+            data:books
+        })
+    }catch(error){
+        console.log(error.message)
+        response.status(500).send({message:error.message})
+    }
+})
 
+app.get('/books/:id',async(request,response)=>{
+    try{
+        const {id}=request.params;
+        const book=await Book.findById(id);
+        return response.status(200).json(book)
+    }catch(error){
+        console.log(error.message)
+        response.status(500).send({message:error.message})
+    }
+})
 mongoose.connect(mongoDBURL).then(()=>{
     console.log('App connected to database');
     app.listen(PORT,()=>{
