787bbd014bb7217a1202a182e628639899626c7d
Andre Adipraja <nielraja123@gmail.com>
2024-02-16 12:23:26 +0700

    Add MongoDB and Mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://root:root@bookstoredb.xduugrl.mongodb.net/BookStore?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 import express, { response } from "express";
-import {PORT} from "./config.js"
+import {PORT, mongoDBURL} from "./config.js"
+import mongoose from "mongoose";
 const app = express();
 app.get('/',(request,response)=>{
     console.log(request)
     return response.status(234).send('Testing')
 })
-app.listen(PORT,()=>{
-    console.log(`App is listening to port: ${PORT}`);
+mongoose.connect(mongoDBURL).then(()=>{
+    console.log('App connected to database');
+    app.listen(PORT,()=>{
+        console.log(`App is listening to port: ${PORT}`);
+    });
+}).catch((error)=>{
+    console.log(error);
 })
