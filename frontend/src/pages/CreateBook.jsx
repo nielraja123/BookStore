4798078636892477983044ded78f625835794915
Andre Adipraja <nielraja123@gmail.com>
2024-02-19 22:55:24 +0700

    Add delete feature, cards, modals, notistack

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
+import { useSnackbar } from 'notistack';
 
 const CreateBook = () => {
   const [title, setTitle]=useState('');
@@ -10,6 +11,7 @@ const CreateBook = () => {
   const [publishedYear, setPublishedYear]=useState('')
   const [loading, setLoading]=useState(false)
   const navigate = useNavigate();
+  const {enqueueSnackbar}=useSnackbar()
   const handleSaveBook = ()=>{
     const data = {
       title,author,publishedYear
@@ -17,10 +19,11 @@ const CreateBook = () => {
     setLoading(true)
     axios.post('http://localhost:5555/books',data).then(()=>{
       setLoading(false)
+      enqueueSnackbar('Book Create Succesfully!', {variant:'success'})
       navigate('/')
     }).catch((error)=>{
       setLoading(false)
-      alert('An error happened, please check the console')
+      enqueueSnackbar('Error!', {variant:'error'})
       console.log(error)
     })
   }
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
+import { useSnackbar } from 'notistack';
 
 const EditBook = () => {
   const [title, setTitle]=useState('');
@@ -11,6 +12,7 @@ const EditBook = () => {
   const [loading, setLoading]=useState(false)
   const navigate = useNavigate();
   const {id} = useParams()
+  const {enqueueSnackbar}= useSnackbar()
   useEffect(()=>{
     setLoading(true)
     axios.get(`http://localhost:5555/books/${id}`).then((response)=>{
@@ -30,11 +32,12 @@ const EditBook = () => {
     };
     setLoading(true)
     axios.put(`http://localhost:5555/books/${id}`,data).then(()=>{
+      enqueueSnackbar('Edit Create Succesfully!', {variant:'success'})
       setLoading(false)
       navigate('/')
     }).catch((error)=>{
       setLoading(false)
-      alert('An error happened, please check the console')
+      enqueueSnackbar('Error!', {variant:'error'})
       console.log(error)
     })
   }
